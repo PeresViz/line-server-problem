05b30a81dc9055ab3b26e14978f04169f3ec44b0
peres <tiago.sousa@viz.ai>
2024-02-17 21:13:45 +0000

    Update build.sh file

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Create a virtual environment
+python -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
 # Install dependencies
 pip install -r requirements.txt
 
